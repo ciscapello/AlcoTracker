fee5f82b7efc0b3118f56949d86c79d33360f4dc
Vladimir Bessonov <ciscapello@gmail.com>
2023-09-02 19:50:01 +0600

    Initial Commit

---
+++ AlcoTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlcoTracker
+//
+//  Created by Владимир on 02.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
